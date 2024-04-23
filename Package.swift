a595d0ebbbd23337c519d34c4d64a941c7b4510e
Ivo <ivo@amie.so>
2024-04-23 18:40:19 +0200

    Use module bundle

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "RRuleSwift"
+            name: "RRuleSwift",
+            resources: [.process("lib")]
         ),
         .testTarget(
             name: "RRuleSwiftTests",
